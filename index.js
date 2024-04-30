33d17fa9c214b1c83512b5c9990fcfe2a3383a0b
Ademola Afolabi <afolabiademola27@gmail.com>
2024-04-30 12:25:35 +0100

    env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const userRouter  = require("./Routes/user.Route")
 const mongoose = require("mongoose");
 const cors = require("cors");
-let uri = "mongodb+srv://afolabiademola27:Collins5@cluster0.1auibv4.mongodb.net/march-db?retryWrites=true&w=majority&appName=Cluster0";
+let uri = env.URL
 
 app.use(cors());
 app.use(express.json({ limit: "50mb"}))
